c72b9265ab4ae12ecf9c2ad953ddc426642d569e
Ma6al1 <magali.duranona@hotmail.com>
2022-09-27 22:08:33 -0300

    Desafio 1 de variables

---
+++ LastName_FirstName_JS_CrearVariables.js
@@ -0,0 +1,8 @@
+// Para el parque tematico, cree 2 variables. Una para la edad y la otra para la altura, ambas expresadas en numeros
+
+var age = 10;
+var height = 100;
+
+/* En el caso de la altura los 100 equivalen a centimetros 
+100 centimetros = 1 metro */
+
