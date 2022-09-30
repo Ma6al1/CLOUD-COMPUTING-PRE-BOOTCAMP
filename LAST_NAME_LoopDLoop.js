c776c13a2739e9380890f63dc0e83f49505df7fb
Ma6al1 <magali.duranona@hotmail.com>
2022-09-30 02:43:07 -0300

    Termine los bucles

--- LAST_NAME_LoopDLoop.js
+++ LAST_NAME_LoopDLoop.js
@@ -11,3 +11,14 @@ for (recorrido = 0; recorrido < 10; recorrido++){
     }
     /* En el condicional se expresa que si el recorrido que se realizo es mayor o igual a 3 se imprima en la consola el mensaje que ofrece el caramelo */
 }
+// Característica Stretch 1 
+
+/* Para poder indicar que solo se otorgue el caramelo a partir de los 3km  y si va a una velocidad de 9km, cree la variable velocidad */
+
+let velocidad = 9;
+
+for (recorrido=0; recorrido < 10 ; recorrido++){
+    if (recorrido>=3 && velocidad >= 9){
+        console.log(`Toma, un caramelo por recorrer ${recorrido} kilometros tan rapido!`)
+    }
+}
