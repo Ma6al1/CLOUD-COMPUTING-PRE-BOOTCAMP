b7758f2a81b8635c4e3e6fa4ee7fdd1105863fa6
Ma6al1 <magali.duranona@hotmail.com>
2022-09-29 22:44:13 -0300

    Cree el bucle

---
+++ LAST_NAME_LoopDLoop.js
@@ -0,0 +1,9 @@
+/* Sabemos que necesitamos un bucle ya que */
+
+let recorrido = 0;
+
+for (recorrido = 1; recorrido < 10; recorrido++){
+    if(recorrido>=3){
+        console.log(`Toma, un caramelo por recorrer ${recorrido} kilometros ¡Felicidades!`)
+    }
+}
