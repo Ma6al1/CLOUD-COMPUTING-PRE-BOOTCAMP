d827db462f5f1d34e18d701bb55ba20be0c0f64b
Ma6al1 <magali.duranona@hotmail.com>
2022-09-29 23:17:14 -0300

    Comente las partes del bucle

--- LAST_NAME_LoopDLoop.js
+++ LAST_NAME_LoopDLoop.js
@@ -1,9 +1,13 @@
-/* Sabemos que necesitamos un bucle ya que */
+
+
+/* Sabemos que necesitamos un bucle ya que es necesario que se repita una accion */
 
 let recorrido = 0;
 
-for (recorrido = 1; recorrido < 10; recorrido++){
+for (recorrido = 0; recorrido < 10; recorrido++){
+    /* en la primera expresion se indica en que valor inicia el recorrido, en la segunda se indica que si el recorrido es mayor a 10 se corte el ciclo y en la tercera expresion actualizamos el valor incrementandolo para generar el bucle*/
     if(recorrido>=3){
         console.log(`Toma, un caramelo por recorrer ${recorrido} kilometros ¡Felicidades!`)
     }
+    /* En el condicional se expresa que si el recorrido que se realizo es mayor o igual a 3 se imprima en la consola el mensaje que ofrece el caramelo */
 }
